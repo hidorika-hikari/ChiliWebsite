f5e71f54d108dd6c270d511e2b5dfe15b635e0e8
hidorika-hikari <promvet_khiawsa@cmu.ac.th>
2025-05-29 10:40:01 +0700

    Finish #21

--- server/routes/products.js
+++ server/routes/products.js
@@ -49,7 +49,7 @@ router.post('/create', async (req, res) => {
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
-        image: imgurl, // <-- fix: use `imgurl` not req.body.images
+        images: imgurl,
         brands: req.body.brands,
         price: req.body.price,
         category: req.body.category,
@@ -65,6 +65,78 @@ router.post('/create', async (req, res) => {
     }
 
     res.status(201).json(product);
+});
+
+
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+});
+
+router.delete('/:id', async (req, res) => {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+
+    if (!deleteProduct) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, message: "Product deleted successfully" });
+});
+
+router.put('/:id', async (req, res) => {
+
+    const limit = pLimit(2);
+
+    const imagesToUpload = req.body.images.map((image) => {
+        return limit(async () => {
+            const result = await cloudinary.uploader.upload(image);
+            return result;
+        });
+    });
+
+    const uploadStatus = await Promise.all(imagesToUpload);
+    const imgurl = uploadStatus.map((item) => item.secure_url);
+
+    if (!uploadStatus) {
+        return res.status(500).json({
+            error: "Images couldn't be uploaded",
+            status: false
+        });
+    }
+    
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            images: imgurl,
+            brands: req.body.brands,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured
+        },
+        { new: true }
+    );
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product cant be updated"
+        });
+    }
+
+    res.status(200).json({
+        message: "Product updated successfully",
+        success: true,
+    });
 });
 
 module.exports = router;
